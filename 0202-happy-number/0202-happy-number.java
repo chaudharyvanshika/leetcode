5cb5535e000aecaee6263d765ce2d66237e47d1a
chaudharyvanshika <chaudhary1shika2848@gmail.com>
2026-09-21 14:53:59 +0530

    Time: 0 ms (100%), Space: 42.6 MB (34.23%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int sumOfSquares(int n){
+        int sum=0;
+        while(n!=0){
+            int digit = n%10;
+            sum += digit*digit;
+            n=n/10;
+        }
+        return sum;
+    }
+
+    public boolean isHappy(int n) {
+        while (n != 1 && n!=16) {
+            n = sumOfSquares(n);
+    }
+    return n==1;
+    }
+
+}
